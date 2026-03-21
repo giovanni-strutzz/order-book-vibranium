2eeedf25d02673cba0888c2f0f43a2367b7524f9
giovanni-strutzz <strutzz.giovanni@gmail.com>
2026-03-21 12:26:11 -0300

    feat: Inital commit with Hexagonal structure and dependencies

---
+++ src/main/java/com/br/strutz/order_book/OrderBookApplication.java
@@ -0,0 +1,13 @@
+package com.br.strutz.order_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderBookApplication.class, args);
+	}
+
+}
